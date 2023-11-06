d1e8fed57fff0174ec31f74496d248e6aa042825
Cindy Chen <xchen@lcs.on.ca>
2023-11-06 15:24:29 -0500

    Initial Commit

---
+++ Reminders App/Reminders_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_AppApp.swift
+//  Reminders App
+//
+//  Created by Cindy Chen on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
